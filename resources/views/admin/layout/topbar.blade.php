fd520946a577eb9399bd2b0b8a7ddeeffa32e6db
HugoMontes <hugomontes927@gmail.com>
2021-07-12 17:09:48 -0400

    Botón cerrar sesión

--- resources/views/admin/layout/topbar.blade.php
+++ resources/views/admin/layout/topbar.blade.php
@@ -155,7 +155,7 @@
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>                             
+                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ Auth::user()->name }}</span>                             
                                 {{ HTML::image('admin/img/undraw_profile.svg', 'User', array('class' => 'img-profile rounded-circle')) }}
                             </a>
                             <!-- Dropdown - User Information -->
@@ -176,7 +176,7 @@
                                 <div class="dropdown-divider"></div>
                                 <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
                                     <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Logout
+                                    Cerrar sesión
                                 </a>
                             </div>
                         </li>
